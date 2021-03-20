fa224a6b19d8c72e96fc320265c3cbb194697f9b
Oren Eden <oren.eden@vonage.com>
2021-03-20 15:18:37 +0200

    add dbService provider

--- src/photos-handler/photos-handler.module.ts
+++ src/photos-handler/photos-handler.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
 import { PhotosHandlerController } from './photos-handler.controller';
 import { PhotosHandlerService } from './photos-handler.service';
 
 @Module({
   controllers: [PhotosHandlerController],
-  providers: [PhotosHandlerService]
+  providers: [PhotosHandlerService, DbService]
 })
 export class PhotosHandlerModule {}
